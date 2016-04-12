be4743f034d5855725a416a8b947a3a6140472c8
khalilgharbaoui <kaygeee@gmail.com>
2016-04-12 22:58:01 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('it works');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
+import App from './components/App';
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      {/*<IndexRoute component={App} />*/}
+    </Route>
+  </Router>
+
+), document.getElementById('root'));
